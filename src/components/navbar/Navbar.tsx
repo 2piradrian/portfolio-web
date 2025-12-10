1a286dfa87afff3dc4bdab249b14f5a2e157ca9d
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-12-10 20:20:06 -0300

    fix: update package-lock.json for dependency version upgrades, reorder skills in skills.json, and adjust navbar styles for better responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,7 +7,6 @@ export default function Navbar() {
 			<ul className={style.navList}>
 				<NavItem href="/#contact" arialabel="Contacto" title="Contacto" />
 				<NavItem href="/#experience" arialabel="Experiencia" title="Experiencia" />
-				<NavItem href="/#projects" arialabel="Proyectos" title="Proyectos" />
 				<NavItem href="/#education" arialabel="Educación" title="Educación" />
 			</ul>
 		</div>
