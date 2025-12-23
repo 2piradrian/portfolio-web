976a44656c8f724cd3de96d976c8cff3e1a9ef5b
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-12-23 15:32:02 -0300

    refactor: remove About and Contact sections, update Hero section with new link

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-import world from "../../assets/page/world.svg";
 import mail from "../../assets/page/mail2.svg";
+import world from "../../assets/page/world.svg";
 import style from "./style.module.css";
 
 export default function Hero() {
 	return (
 		<section className="container" id="contact">
-			<h1 className={style.name}>Adrián Rodriguez</h1>
-			<p className={style.about}>Desarrollador de Software | Estudiante de ingeniería</p>
+			<h1 className={style.name}>
+				Adrián Rodriguez
+			</h1>
+			<p className={style.about}>
+				Desarrollador de Software | Estudiante de ingeniería
+			</p>
 			<div className={style.location}>
 				<Image src={world} alt="Residencia" />
 				<p>Córdoba, Argentina.</p>
@@ -20,6 +24,7 @@ export default function Hero() {
 			<div className={style.contact}>
 				<Link href="https://www.linkedin.com/in/rodriguezcadr/">Linkedin</Link>
 				<Link href="https://github.com/2piradrian">Github</Link>
+				<Link href="https://docs.google.com/spreadsheets/d/1YmP0-QebjkGPvkTo9ntigc7-bBILuzYB6fnYYnwU1IY/edit?gid=0#gid=0">Calificaciones</Link>
 			</div>
 		</section>
 	);
